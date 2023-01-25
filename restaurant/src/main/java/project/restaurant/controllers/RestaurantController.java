d39fffae7ae5adac4f5ef03ffc26b888579a6b5c
Pengyuan Yan <ykas051@live.rhul.ac.uk>
2023-01-26 04:05:08 +0800

    Add the fields for the Order class then add setter and getter methods for each field and write toString method (with Tanmeet)

--- restaurant/src/main/java/project/restaurant/controllers/RestaurantController.java
+++ restaurant/src/main/java/project/restaurant/controllers/RestaurantController.java
@@ -1,12 +1,20 @@
 package project.restaurant.controllers;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import project.restaurant.models.Order;
 
 @Controller
 public class RestaurantController {
 	@GetMapping("/home")
 	public String getHome() {
 		return "home";
+	}
+	
+	@GetMapping
+	public List<Order> getOrderList() {
+	  return List.of(
+	     (Order) new Order(1,1,1));
 	}
 }
--- restaurant/src/main/java/project/restaurant/models/Order.java
+++ restaurant/src/main/java/project/restaurant/models/Order.java
@@ -3,7 +3,42 @@ package project.restaurant.models;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "Order")
+@Table
 public class Order {
-    
+  
+  private Integer orderNumber;
+  private Integer status;
+  private Integer waiterId;
+  
+  public Order(Integer orderNumber, Integer status, Integer waiterId) {
+    this.orderNumber = orderNumber;
+    this.status = status;
+    this.waiterId = waiterId;
+  }
+  
+  public Integer getOrderNumber() {
+    return orderNumber;
+  }
+  public void setOrderNumber(Integer orderNumber) {
+    this.orderNumber = orderNumber;
+  }
+  
+  public Integer getStatus() {
+    return status;
+  }
+  public void setStatus(Integer status) {
+    this.status = status;
+  }
+  
+  public Integer getWaiterId() {
+    return waiterId;
+  }
+  public void setWaiterId(Integer waiterId) {
+    this.waiterId = waiterId;
+  }
+  
+  @Override
+  public String toString() {
+    return "order number:" + orderNumber.toString() + " status:" + status.toString() + " waiter id:" + waiterId.toString();
+  }
 }

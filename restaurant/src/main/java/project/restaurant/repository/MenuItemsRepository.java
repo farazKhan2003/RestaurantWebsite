9c65d6a52df3a160c46a9f456e1509f7352e9aa4
Gill Peter (2019) ZHAC222 <peter.gill.2019@live.rhul.ac.uk>
2023-01-26 19:38:35 +0000

    Added query for search in menu items repository.

--- restaurant/src/main/java/project/restaurant/repository/MenuItemsRepository.java
+++ restaurant/src/main/java/project/restaurant/repository/MenuItemsRepository.java
@@ -9,4 +9,7 @@ import project.restaurant.models.MenuItems;
 
 @Repository
 public interface MenuItemsRepository extends JpaRepository<MenuItems, Integer>{
+   @Query(value = "SELECT * FROM menu_items m WHERE m.item_name LIKE :keyword OR m.description LIKE :keyword", nativeQuery = true)
+   public List<MenuItems> searchMenuItemsByKeyword(@Param("keyword") String keyword);
+   
 }

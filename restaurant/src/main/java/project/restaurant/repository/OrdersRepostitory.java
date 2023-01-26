aecc304a012f6cb8b84d1559f6ab1706cef628d0
gaz224 <93044515+gaz224@users.noreply.github.com>
2023-01-26 19:35:51 +0000

    Added orders repository file, made sql code for table creation(included in file)

---
+++ restaurant/src/main/java/project/restaurant/repository/OrdersRepostitory.java
@@ -0,0 +1,20 @@
+package project.restaurant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.restaurant.models.Orders;
+
+public interface OrdersRepostitory extends JpaRepository<Orders, Integer> {
+  //@Query("INSERT INTO Orders(confirmed) VALUES(true);")
+  //public Boolean confirmOrder(Boolean confirmed);
+}
+
+//SQL code for test table:
+//
+//DROP DATABASE IF EXISTS cs2810restaurant;
+//CREATE DATABASE IF NOT EXISTS cs2810restaurant;
+//USE cs2810restaurant;
+//CREATE TABLE IF NOT EXISTS `cs2810restaurant`.`Orders` (
+//    `orderId` INT NOT NULL AUTO_INCREMENT,
+//    `confirmed` BOOLEAN NOT NULL,
+//    PRIMARY KEY (`orderId`)
+//)

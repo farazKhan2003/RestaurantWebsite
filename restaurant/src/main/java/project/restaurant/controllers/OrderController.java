9fdd6dacc7a6141cf7a0090dd40696bcc54061ec
Pengyuan Yan <ykas051@live.rhul.ac.uk>
2023-01-27 03:34:02 +0800

    Add new function for OrderController class mapping the order web page with database so that the web page can get the order according to the waiter id

--- restaurant/src/main/java/project/restaurant/controllers/OrderController.java
+++ restaurant/src/main/java/project/restaurant/controllers/OrderController.java
@@ -1,8 +1,13 @@
 package project.restaurant.controllers;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import project.restaurant.models.Orders;
 import project.restaurant.repositories.OrderRepository;
 
 @Controller
@@ -10,9 +15,23 @@ public class OrderController {
 
     @Autowired
     private OrderRepository orderRepository;
-
-    @GetMapping("/Order")
-    public String getOrder() {
-        return "Order";
+    
+    /*@GetMapping("/Order")
+    public List<Order> getOrder() {
+        return List.of(new Order(1,1,1));
+    }*/
+    
+    @GetMapping("/waiterinput")
+    public String getWaiter(@Param("keyword") String keyword, Model model) {
+      Integer waiterId = Integer.parseInt(keyword);
+      List<Orders> orders = orderRepository.findByWaiterId(waiterId);
+      model.addAttribute("orders", orders);
+      return "order";
+    }
+    
+    
+    @GetMapping("/order")
+    public String getOrders() {
+      return "order";
     }
 }

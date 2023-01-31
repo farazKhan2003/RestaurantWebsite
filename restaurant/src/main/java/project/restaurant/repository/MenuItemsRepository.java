bae73ad5663d5cecafdcd5d933855d22473f6157
Martin James 2 (2021) ZKAC235 <james.martin.2.2021@live.rhul.ac.uk>
2023-01-31 14:33:17 +0000

    MenuItemsRepository interface extending JpaRepository for querying mysql using MenuItems model

---
+++ restaurant/src/main/java/project/restaurant/repository/MenuItemsRepository.java
@@ -0,0 +1,15 @@
+package project.restaurant.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import project.restaurant.models.MenuItems;
+
+@Repository
+public interface MenuItemsRepository extends JpaRepository<MenuItems, Integer>{
+   @Query(value = "SELECT * FROM menu_items m WHERE m.item_name LIKE :keyword OR m.descriptions LIKE :keyword", nativeQuery = true)
+   public List<MenuItems> searchMenuItemsByKeyword(@Param("keyword") String keyword);
+   
+}

8359772212e9469ce178f8616c22e1b6c8f315ef
Gill Peter (2019) ZHAC222 <peter.gill.2019@live.rhul.ac.uk>
2023-01-31 14:25:05 +0000

    User model mimicking database structure.

---
+++ restaurant/src/main/java/project/restaurant/models/Users.java
@@ -0,0 +1,79 @@
+package project.restaurant.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "Users")
+public class Users {
+  
+  public Users() {}
+  
+  public Users(String usertype, String username, String passwords, String email_address) {
+    this.usertype = usertype;
+    this.username = username;
+    this.passwords = passwords;
+    this.email_address = email_address;
+  }
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer userid;
+  
+  @Column(name = "usertype", nullable = false)
+  private String usertype;
+  
+  @Column(name = "username", nullable = false)
+  private String username;
+  
+  @Column(name = "passwords", nullable = false, length = 64)
+  private String passwords;
+  
+  @Column(name = "email_address", nullable = false)
+  private String email_address;
+  
+  @OneToMany(cascade = CascadeType.ALL)
+  private Set<Orders> orders;
+
+  public Integer getUserid() {
+    return userid;
+  }
+
+  public void setUserid(Integer userid) {
+    this.userid = userid;
+  }
+
+  public String getUsertype() {
+    return usertype;
+  }
+
+  public void setUsertype(String usertype) {
+    this.usertype = usertype;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPasswords() {
+    return passwords;
+  }
+
+  public void setPasswords(String passwords) {
+    this.passwords = passwords;
+  }
+}

f801dd4d2b32e1bcb1f02f3548eb5dbf82209485
Tanmeet Gabba (zkac335) <zkac335@live.rhul.ac.uk>
2023-02-01 19:55:12 +0000

    Adding required fields to Basket model with corresponding Springboot annotations

--- restaurant/src/main/java/project/restaurant/models/Basket.java
+++ restaurant/src/main/java/project/restaurant/models/Basket.java
@@ -1,4 +1,20 @@
 package project.restaurant.models;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Basket")
 public class Basket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<MenuItems> items = new ArrayList<>();
+
+    Float totalPrice;
 }

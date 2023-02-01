e9f4393f3e514d494a29834bf2e1eba3b680145f
Pengyuan Yan <ykas051@live.rhul.ac.uk>
2023-02-01 09:17:07 +0800

    Add first test for testForOrdersService class to test the getOrders method of it

--- restaurant/src/test/java/project/restaurant/TestForOrdersService.java
+++ restaurant/src/test/java/project/restaurant/TestForOrdersService.java
@@ -1,13 +1,23 @@
 package project.restaurant;
 
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import jakarta.annotation.Resource;
+import project.restaurant.models.Orders;
+import project.restaurant.services.OrderService;
 
 @SpringBootTest
 class TestForOrdersService {
-
+  
+  @Resource
+  private OrderService orderService;
+  
   @Test
   void test() {
+    List<Orders> list = orderService.getOrders();
+    System.out.println(list);
   }
 
 }

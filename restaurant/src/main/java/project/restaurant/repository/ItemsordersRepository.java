cf79ac5767ee200096fc55862c081e0a72889701
Pengyuan Yan <ykas051@live.rhul.ac.uk>
2023-02-03 01:43:29 +0800

    Make the itemorderRepository interface act to offer the corresponding method save() to save the custome's chose to database

--- restaurant/src/main/java/project/restaurant/repository/ItemsordersRepository.java
+++ restaurant/src/main/java/project/restaurant/repository/ItemsordersRepository.java
@@ -1,11 +1,13 @@
 package project.restaurant.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-//import project.restaurant.models.Itemsorders;
+import project.restaurant.models.ItemsOrders;
 
-//@Repository
-//public interface ItemsordersRepository extends JpaRepository<Itemsorders,Integer>{
-//
-//}
+@Repository
+public interface ItemsordersRepository extends JpaRepository<ItemsOrders,Integer>{
+  @Query("")
+  public void save(Integer gID);
+}

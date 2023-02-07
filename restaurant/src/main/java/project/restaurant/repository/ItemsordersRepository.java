312f3923817f2d49cadafd7299221a35fb727399
Pengyuan Yan <ykas051@live.rhul.ac.uk>
2023-02-08 04:01:35 +0800

    Add 5 queries in three different repository to get the name of the different menuitem, the total number of menuitem, and the total price of menuitem

--- restaurant/src/main/java/project/restaurant/repository/ItemsordersRepository.java
+++ restaurant/src/main/java/project/restaurant/repository/ItemsordersRepository.java
@@ -1,11 +1,15 @@
 package project.restaurant.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import project.restaurant.models.ItemsOrders;
+import project.restaurant.models.Orders;
 
 @Repository
 public interface ItemsordersRepository extends JpaRepository<ItemsOrders,Integer>{
+  @Query("SELECT o FROM ItemsOrders o WHERE orderid = ?1")
+  List<ItemsOrders> findAllItemOrders(Orders orders);
 }
--- restaurant/src/main/java/project/restaurant/repository/OrdersRepository.java
+++ restaurant/src/main/java/project/restaurant/repository/OrdersRepository.java
@@ -1,17 +1,21 @@
 package project.restaurant.repository;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import project.restaurant.models.Orders;
+import project.restaurant.models.Users;
 
 @Repository
 public interface OrdersRepository extends JpaRepository<Orders, Integer>{
 	@Query("SELECT o FROM Orders o WHERE o.state != 3")
 	public List<Orders> findByState();
+	
+	@Query("SELECT o FROM Orders o WHERE o.userid = ?1")
+    public List<Orders> findByUID(Optional<Users> user);
     
 	//@Query("SELECT o FROM Orders o WHERE o.orderId = ?1")
 	//public List<Orders> findByOrderId(Integer orderId);

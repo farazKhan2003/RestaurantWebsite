bb6ef9d42e5d76136cd0dcddea457c2b24743f3f
Martin James 2 (2021) ZKAC235 <james.martin.2.2021@live.rhul.ac.uk>
2023-02-09 19:50:04 +0000

    Delete BasketRepository.java depreciated implementation of database manipulation of basket

--- restaurant/src/main/java/project/restaurant/repository/BasketRepository.java
+++
@@ -1,9 +0,0 @@
-package project.restaurant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import project.restaurant.models.Basket;
-
-//@Repository
-//public interface BasketRepository extends JpaRepository<Basket, Integer>{
-//}

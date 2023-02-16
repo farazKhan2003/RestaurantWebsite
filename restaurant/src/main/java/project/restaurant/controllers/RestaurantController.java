ecac7da8d9f6cdbe43d332fa133e07bff66317a4
irmani jassal <zjac293@live.rhul.ac.uk>
2023-02-16 23:27:53 +0300

    added mapping for order page

--- restaurant/src/main/java/project/restaurant/controllers/RestaurantController.java
+++ restaurant/src/main/java/project/restaurant/controllers/RestaurantController.java
@@ -36,4 +36,10 @@ public class RestaurantController {
         return "ordering-menu";
     }
 	
+	//TEMP MAPPING WILL BE CHANGED LATER FOR FUNCTIONALITY.
+    @GetMapping("/waiter-orders")
+    public String getOrders() {
+        return "waiter-orders";
+    }
+	
 }

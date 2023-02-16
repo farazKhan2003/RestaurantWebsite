0d5a5c2a63337d7847569de604a115bb7c44603b
Pengyuan Yan <ykas051@live.rhul.ac.uk>
2023-02-17 03:53:35 +0800

    Add a placeDeliveryOrder method to get all the order ready for delivering for the first list of orders html web page

--- restaurant/src/main/java/project/restaurant/controllers/OrderStateController.java
+++ restaurant/src/main/java/project/restaurant/controllers/OrderStateController.java
@@ -1,7 +1,10 @@
 package project.restaurant.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import project.restaurant.models.ItemsOrders;
 
 @Controller
 public class OrderStateController {
@@ -10,5 +13,13 @@ public class OrderStateController {
     public String getOrders() {
         return "orders";
     }
-
+    
+    @PostMapping("/getDeliveryStateOrder")
+    public String placeDeliveryOrder(Model model) {
+      
+      ItemsOrders deliveryStateOrder = new ItemsOrders();
+      
+      model.addAttribute("deliveryStateOrder", deliveryStateOrder);
+      return "orders";
+    }
 }

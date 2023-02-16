1ff5abe744a8f9ce9cfcb9df17a35f4608255268
wkis281 <WKIS281@live.rhul.ac.uk>
2023-02-17 03:40:33 +0800

    Set the orderstatecontroller class as the controller class and add a getorder to connect to the order html page.

--- restaurant/src/main/java/project/restaurant/controllers/OrderStateController.java
+++ restaurant/src/main/java/project/restaurant/controllers/OrderStateController.java
@@ -1,4 +1,14 @@
 package project.restaurant.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class OrderStateController {
+
+    @GetMapping("/orders")
+    public String getOrders() {
+        return "orders";
+    }
+
 }

f9fb10df16f2b27e8fa477dc30693e9513b3eaa8
Gill Peter (2019) ZHAC222 <peter.gill.2019@live.rhul.ac.uk>
2023-02-23 17:43:06 +0000

    Added query to search help requests by the id of the customer who made the request.

--- restaurant/src/main/java/project/restaurant/repository/HelpRepository.java
+++ restaurant/src/main/java/project/restaurant/repository/HelpRepository.java
@@ -1,10 +1,14 @@
 package project.restaurant.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import project.restaurant.models.Helps;
 
 @Repository
-public interface HelpRepository extends JpaRepository<Helps, Integer> {
-
+public interface HelpRepository extends JpaRepository<Helps, Integer>{
+	@Query("SELECT h FROM Helps h WHERE h.userid =?1")
+	public Helps SearchHelpByUserId(@Param("userid") Integer userid);
 }

ae1f077a249e2f73466ac3966df60ca8d7f28a04
Khan Faraz (2021) ZKAC354 <faraz.khan.2021@live.rhul.ac.uk>
2023-02-23 17:48:24 +0000

    Added methods for waiter to update the status of help requests, added functionality to display help requests for the HTML page

--- restaurant/src/main/java/project/restaurant/controllers/HelpController.java
+++ restaurant/src/main/java/project/restaurant/controllers/HelpController.java
@@ -1,13 +1,16 @@
 package project.restaurant.controllers;
 
-import jakarta.servlet.http.HttpSession;
-import org.apache.coyote.Request;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
+
+import jakarta.servlet.http.HttpSession;
 import project.restaurant.models.Helps;
 import project.restaurant.models.Users;
 import project.restaurant.repository.HelpRepository;
@@ -18,18 +21,46 @@ public class HelpController {
     private HelpRepository hRepo;
 
     @GetMapping("/help")
-    public String getHelp(Model model) {
+    public String getHelp(HttpSession session) {
+        Users user = (Users) session.getAttribute("user");
+        if (hRepo.SearchHelpByUserId(user.getUserid()) != null) {
+            return "helpRequested";
+        }
         return "help";
     }
 
-
-
+    @GetMapping("/waiterhelps")
+    public String getWaiterHelps(Model model) {
+        List<Helps> lHelps = hRepo.findAll();
+        model.addAttribute("helps",lHelps);
+        return "waiterhelp";
+    }
     @PostMapping("/helpRequest")
     public String requestHelp(HttpSession session) {
         Users user = (Users) session.getAttribute("user");
+        if (hRepo.SearchHelpByUserId(user.getUserid()) != null) {
+            return "helpRequested";
+        }
         Helps newRequest = new Helps("needs help", user.getUserid());
-        System.out.println(user.getUsername() + user.getPasswords() + user.getUserid() + user.getUsertype());
         hRepo.save(newRequest);
         return "helpRequested";
+    }
+
+    @GetMapping("/accepthelp")
+    public String acceptHelp(@RequestParam("helpid") Integer helpid, Model model) {
+        Helps helpRequest = hRepo.getReferenceById(helpid);
+        helpRequest.setState("helping");
+        hRepo.save(helpRequest);
+        List<Helps> lHelps = hRepo.findAll();
+        model.addAttribute("helps",lHelps);
+        return "waiterhelp";
+    }
+
+    @GetMapping("/closehelp")
+    public String closeHelp(@RequestParam("helpid") Integer helpid, Model model) {
+        hRepo.deleteById(helpid);
+        List<Helps> lHelps = hRepo.findAll();
+        model.addAttribute("helps",lHelps);
+        return "waiterhelp";
     }
 }

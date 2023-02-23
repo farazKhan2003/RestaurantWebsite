26dfd4228e4a7ea0f23cbf39030d068787d8cb52
bailey caplan <zkac236@live.rhul.ac.uk>
2023-02-23 17:38:33 +0000

    rewritten OrdersRepository

--- restaurant/src/main/java/project/restaurant/repository/OrdersRepository.java
+++ restaurant/src/main/java/project/restaurant/repository/OrdersRepository.java
@@ -4,8 +4,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+import project.restaurant.models.MenuItems;
 import project.restaurant.models.Orders;
 import project.restaurant.models.Users;
 
@@ -13,10 +14,13 @@ import project.restaurant.models.Users;
 public interface OrdersRepository extends JpaRepository<Orders, Integer>{
 	@Query("SELECT o FROM Orders o WHERE o.state != 3")
 	public List<Orders> findByState();
-	
+
 	@Query("SELECT o FROM Orders o WHERE o.userid = ?1")
-    public List<Orders> findByUID(Optional<Users> user);
-    
-	//@Query("SELECT o FROM Orders o WHERE o.orderId = ?1")
-	//public List<Orders> findByOrderId(Integer orderId);
+	public List<Orders> findByUID(Optional<Users> user);
+
+	@Query(value = "SELECT DISTINCT orderid FROM orders", nativeQuery = true)
+	public List<String> findAllDistinctID();
+
+	@Query("SELECT o FROM Orders o WHERE o.state != ?1")
+	public List<Orders> findByState(String state);
 }

8d0192587ceee132c175e13881830ae8dca983c0
Gill Peter (2019) ZHAC222 <peter.gill.2019@live.rhul.ac.uk>
2023-02-23 13:48:36 +0000

    Update LoginController.java to support different types of user logging in and out

--- restaurant/src/main/java/project/restaurant/controllers/LoginController.java
+++ restaurant/src/main/java/project/restaurant/controllers/LoginController.java
@@ -5,29 +5,55 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
 import project.restaurant.models.Users;
+import project.restaurant.models.Waiters;
 import project.restaurant.repository.UsersRepository;
+import project.restaurant.repository.WaitersRepository;
 
 @Controller
 public class LoginController {
     @Autowired
     private UsersRepository uRepo;
 
+    @Autowired
+    private WaitersRepository wRepo;
+    
     @GetMapping("/login")
     public String getLogin() {
         return "login";
     }
 
     @PostMapping("/confirmLogin")
-    public String confirmLogin(@RequestParam("username") String userName, @RequestParam("password") String password) {
-        System.out.println("Start");
+    public String confirmLogin(HttpSession session, @RequestParam("username") String userName, @RequestParam("password") String password) {
         Users user = uRepo.SearchUsersByUserOrEmailAndPassword(userName, password);
-        System.out.println("hello");
         if (user == null) {
-            System.out.println("if reached");
             return "loginFailed";
         }
-        System.out.println("returning home");
-        return "home";
+        Waiters waiter = wRepo.SearchWaitersByUserId(user);
+        session.setAttribute("user",user);
+        if(waiter==null) {
+        	return "home";
+        }
+        session.setAttribute("waiter", waiter);
+        return "waiterhome";
+    }
+    
+    @PostMapping("/logout")
+    public String logout(HttpSession session) {
+    	session.removeAttribute("user");
+    	if(session.getAttribute("waiter")!=null) {
+    		session.removeAttribute("waiter");
+    	}
+    	return "home";
+    }
+    
+    @GetMapping("/waiterhome")
+    public String waiterhome(HttpSession session){
+    	if(session.getAttribute("waiter")==null) {
+    		return "home";
+    	}
+    	return "waiterhome";
     }
 }

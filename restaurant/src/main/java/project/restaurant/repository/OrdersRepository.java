0d64a92b9f5830d496b0eb00b4ccdd94c21ca966
Martin James 2 (2021) ZKAC235 <james.martin.2.2021@live.rhul.ac.uk>
2023-02-28 23:44:47 +0000

    Update OrdersRepository.java to include sprint 3 modifications

--- restaurant/src/main/java/project/restaurant/repository/OrdersRepository.java
+++ restaurant/src/main/java/project/restaurant/repository/OrdersRepository.java
@@ -4,33 +4,30 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import project.restaurant.models.MenuItems;
+
 import project.restaurant.models.Orders;
 import project.restaurant.models.Users;
 
 @Repository
 public interface OrdersRepository extends JpaRepository<Orders, Integer>{
-	@Query("SELECT o FROM Orders o WHERE o.state != 3")
-	public List<Orders> findByState();
 	
-	@Query("SELECT o FROM Orders o WHERE o.userid = ?1")
+    @Query("SELECT o FROM Orders o WHERE o.state = 'not confirmed'")
+    public List<Orders> findByState();
+
+    @Query("SELECT o FROM Orders o WHERE o.userid = ?1")
     public List<Orders> findByUID(Optional<Users> user);
-	
-	@Query(value = "SELECT DISTINCT orderid FROM orders", nativeQuery = true)
-	public List<String> findAllDistinctID();
-    
 
-	@Query("SELECT o FROM Orders o WHERE o.state != ?1")
-	public List<Orders> findByState2(String state);
-
-	@Query(value = "select * from orders where waiterid=:id", nativeQuery = true)
+    @Query(value = "select * from orders where waiterid=:id", nativeQuery = true)
     public List<Orders> findOrderByWaiterId(int id);
-    
-	@Query(value = "select * from orders where orderid=:input", nativeQuery = true)
+
+    @Query(value = "select * from orders where orderid=:input", nativeQuery = true)
     public Orders findOrderByOrderId(Integer input);
     
-	//@Query("SELECT o FROM Orders o WHERE o.orderId = ?1")
-	//public List<Orders> findByOrderId(Integer orderId);
+    @Query("SELECT o FROM Orders o WHERE o.orderid = ?1")
+    public List<Orders> findByOrderId(Integer orderId);
+    
+	@Query("SELECT o FROM Orders o WHERE o.state != ?1")
+	public List<Orders> findByState(String state);
+
 }

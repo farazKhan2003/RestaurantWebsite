55a91740d45cc0a634ddd6ee3664cb4d2ea9b045
Martin James 2 (2021) ZKAC235 <james.martin.2.2021@live.rhul.ac.uk>
2023-02-28 20:32:56 +0000

    Update MenuViewController.java sprint 3 complete work

--- restaurant/src/main/java/project/restaurant/controllers/MenuViewController.java
+++ restaurant/src/main/java/project/restaurant/controllers/MenuViewController.java
@@ -1,60 +1,59 @@
 package project.restaurant.controllers;
 
-import java.io.IOException;
-import java.util.List;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import project.restaurant.models.MenuItems;
+
+import jakarta.servlet.http.HttpSession;
 import project.restaurant.models.Users;
 import project.restaurant.models.Waiters;
-import project.restaurant.repository.MenuItemsRepository;
 import project.restaurant.repository.UsersRepository;
 import project.restaurant.repository.WaitersRepository;
 
 @Controller
-public class MenuViewController {
+public class LoginController {
+    @Autowired
+    private UsersRepository uRepo;
 
-  @Autowired
-  private MenuItemsRepository mRepo;
+    @Autowired
+    private WaitersRepository wRepo;
+    
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
 
-  @Autowired
-  private UsersRepository uRepo;
-
-  @Autowired
-  private WaitersRepository wRepo;
-  
-
-  @GetMapping("/orderingmenu") 
-  public String getMenuView(Model model) {
-    List<MenuItems> menuItems = mRepo.findAll();
-    model.addAttribute("menuItems", menuItems);
-    List<String> cat = mRepo.findAllDistinctCat();
-    model.addAttribute("cat", cat); 
-      return "orderingmenu";
-  }
-  
-  @PostMapping("/addItem2")
-  public String postAddItem(@RequestParam("file") MultipartFile file) throws IOException {
-    Users u = new Users("qwe1","qwe2","qwe3","qwe4");
-    uRepo.save(u);
-    Waiters waiter = new Waiters(1, u);
-    wRepo.save(waiter);
-    Float f = 1.25f;
-    byte[] thisArray = file.getBytes();
-    String fle = Base64.encodeBase64String(thisArray);
-    MenuItems m = new MenuItems("ItemName1", "this is a description this is a description this is a description this is a descriptionthis is a description this is a description this is a descriptionthis is a descriptionthis is a description this is a description this is a description this is a description this is a descriptionthis is a descriptionthis is a description this is a description", f, fle , 1, waiter, "drinks", "Paprika, Curry Sauce", 450);
-    mRepo.save(m);
-    return "insert";
-  }
-  
-  @GetMapping("/insert")
-  public String getInserts() {
-    return "insert";
-  }
+    @PostMapping("/confirmLogin")
+    public String confirmLogin(HttpSession session, @RequestParam("username") String userName, @RequestParam("password") String password) {
+        Users user = uRepo.SearchUsersByUserOrEmailAndPassword(userName, password);
+        if (user == null) {
+            return "loginFailed";
+        }
+        Waiters waiter = wRepo.SearchWaitersByUserId(user);
+        session.setAttribute("user",user);
+        if(waiter==null) {
+        	return "home";
+        }
+        session.setAttribute("waiter", waiter);
+        return "waiterhome";
+    }
+    
+    @PostMapping("/logout")
+    public String logout(HttpSession session) {
+    	session.removeAttribute("user");
+    	if(session.getAttribute("waiter")!=null) {
+    		session.removeAttribute("waiter");
+    	}
+    	return "home";
+    }
+    
+    @GetMapping("/waiterhome")
+    public String waiterhome(HttpSession session){
+    	if(session.getAttribute("waiter")==null) {
+    		return "home";
+    	}
+    	return "waiterhome";
+    }
 }

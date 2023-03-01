9640185a3cdbf20303d33912b7ba5c70c4af7f8b
Pengyuan Yan <ykas051@live.rhul.ac.uk>
2023-03-02 00:18:21 +0800

    Add a getmapping method to get the kitchednStaffOrders web page and offer correspongding data to generate list

--- restaurant/src/main/java/project/restaurant/controllers/KitchenStaffController.java
+++ restaurant/src/main/java/project/restaurant/controllers/KitchenStaffController.java
@@ -1,8 +1,11 @@
 package project.restaurant.controllers;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import project.restaurant.models.Orders;
 import project.restaurant.repository.OrdersRepository;
 
 @Controller
@@ -10,5 +13,23 @@ public class KitchenStaffController {
 
     @Autowired
     private OrdersRepository oRepo;
-
+    
+    @GetMapping("/kitchenStaffOrders")
+    public String getWaitForCookOreders(Model model) {
+      
+      List<Orders> ConfirmedStateOrder = oRepo.findConfirmedState();
+      List<Orders> CookingStateOrder = oRepo.findCookingState();
+      List<Orders> ReadyStateOrder = oRepo.findReadyState();
+      
+      System.out.println(ConfirmedStateOrder.size());
+      System.out.println(CookingStateOrder.size());
+      System.out.println(ReadyStateOrder.size());
+      
+      model.addAttribute("ConfirmedStateOrder", ConfirmedStateOrder);
+      model.addAttribute("CookingStateOrder", CookingStateOrder);
+      model.addAttribute("ReadyStateOrder", ReadyStateOrder);
+      
+      return "kitchenStaffOrders";
+    }
+    
 }

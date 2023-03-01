92178cab5481775987793ed4ecc644db502c664f
Pengyuan Yan <ykas051@live.rhul.ac.uk>
2023-03-02 02:52:17 +0800

    Add a name for each table in the kitchenStaffOrders web page

--- restaurant/src/main/java/project/restaurant/controllers/KitchenStaffController.java
+++ restaurant/src/main/java/project/restaurant/controllers/KitchenStaffController.java
@@ -2,7 +2,6 @@ package project.restaurant.controllers;
 
 import java.util.List;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;

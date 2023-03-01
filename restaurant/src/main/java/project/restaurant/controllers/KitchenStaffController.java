bcec9df65fa9cf28eb61f032cbf17b2c6d38da3a
Tanmeet Gabba (zkac335) <zkac335@live.rhul.ac.uk>
2023-03-01 15:14:38 +0000

    Adding controller annotation for Kitchen staff and OrdersRepository field

--- restaurant/src/main/java/project/restaurant/controllers/KitchenStaffController.java
+++ restaurant/src/main/java/project/restaurant/controllers/KitchenStaffController.java
@@ -1,4 +1,14 @@
 package project.restaurant.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import project.restaurant.repository.OrdersRepository;
+
+@Controller
 public class KitchenStaffController {
+
+    @Autowired
+    private OrdersRepository oRepo;
+
 }

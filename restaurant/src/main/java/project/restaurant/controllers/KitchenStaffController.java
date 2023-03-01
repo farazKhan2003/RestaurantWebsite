eb36b0e026c75f42c606b1b732c87020a18d5d37
Tanmeet Gabba (zkac335) <zkac335@live.rhul.ac.uk>
2023-03-01 17:29:07 +0000

    Adding PostMapping for changing the state of an order to "Cooking"

--- restaurant/src/main/java/project/restaurant/controllers/KitchenStaffController.java
+++ restaurant/src/main/java/project/restaurant/controllers/KitchenStaffController.java
@@ -1,10 +1,14 @@
 package project.restaurant.controllers;
 
 import java.util.List;
+
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import project.restaurant.models.Orders;
 import project.restaurant.repository.OrdersRepository;
 
@@ -15,7 +19,7 @@ public class KitchenStaffController {
     private OrdersRepository oRepo;
     
     @GetMapping("/kitchenStaffOrders")
-    public String getWaitForCookOreders(Model model) {
+    public String getKitchenOrders(Model model) {
       
       List<Orders> ConfirmedStateOrder = oRepo.findConfirmedState();
       List<Orders> CookingStateOrder = oRepo.findCookingState();
@@ -30,6 +34,14 @@ public class KitchenStaffController {
       model.addAttribute("ReadyStateOrder", ReadyStateOrder);
       
       return "kitchenStaffOrders";
+    }
+
+    @PostMapping("/changeToCooking")
+    public String changeStateToDelivered(@Param("input") Integer input, Model model) {
+        Orders order = oRepo.findOrderByOrderId(input);
+        order.setState("cooking");
+        oRepo.save(order);
+        return "kitchenStaffOrders";
     }
     
 }

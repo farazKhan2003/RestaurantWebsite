022b25f323eb54fd366a77d397ecad5478fcc98e
Pengyuan Yan <ykas051@live.rhul.ac.uk>
2023-03-02 08:15:20 +0800

    Add a getmapping method getOrderDetail to map the orderdetail web page with this function to show the content of order

--- restaurant/src/main/java/project/restaurant/controllers/KitchenStaffController.java
+++ restaurant/src/main/java/project/restaurant/controllers/KitchenStaffController.java
@@ -1,14 +1,24 @@
 package project.restaurant.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import project.restaurant.models.BasketItemWithId;
+import project.restaurant.models.ItemsOrders;
+import project.restaurant.models.MenuItems;
 import project.restaurant.models.Orders;
+import project.restaurant.repository.ItemsordersRepository;
+import project.restaurant.repository.MenuItemsRepository;
 import project.restaurant.repository.OrdersRepository;
 
 @Controller
@@ -16,6 +26,12 @@ public class KitchenStaffController {
 
     @Autowired
     private OrdersRepository oRepo;
+    
+    @Autowired
+    private ItemsordersRepository iRepo;
+    
+    @Autowired
+    private MenuItemsRepository mRepo;
     
     @GetMapping("/kitchenStaffOrders")
     public String getKitchenOrders(Model model) {
@@ -58,7 +74,76 @@ public class KitchenStaffController {
         System.out.println("****************************************");
         System.out.println(input);
         System.out.println("****************************************");
-        getKitchenOrders(model);
-        return "kitchenStaffOrders";
+        getOrderDetail(input, model);
+        return "orderdetail";
     }
+    
+    @GetMapping("/orderdetail")
+    public String getOrderDetail(@Param("input") Integer input, Model model) {
+      
+      System.out.println("****************************************");
+      System.out.println(input);
+      System.out.println("****************************************");
+      
+      List<ItemsOrders> items = iRepo.findAllItemOrders(oRepo.findOrderByOrderId(input));
+      
+      System.out.println("****************************************");
+      System.out.println(items.size());
+      System.out.println("****************************************");
+      
+      List<Integer> menuitemid = new ArrayList<Integer>();
+      for(int i = 0; i<items.size(); i++) {
+        menuitemid.add(items.get(i).getItemid().getItemid());
+      }
+      
+      System.out.println("****************************************");
+      System.out.println(menuitemid.size());
+      System.out.println("****************************************");
+      
+      Set<Integer> set = new TreeSet<Integer>(menuitemid);
+
+      Integer[] array = new Integer[set.size()];
+
+      Iterator<Integer> iterator = set.iterator();
+
+      int j = 0;
+      while (iterator.hasNext()) {
+          array[j] = iterator.next();
+          j++;
+      }
+
+      Map<Integer, Integer> map = new HashMap<>();
+
+      for (int i = 0; i < menuitemid.size(); i++) {
+          if (map.containsKey(menuitemid.get(i)) == false) {
+              map.put(menuitemid.get(i), 1);
+          } else {
+              map.put(menuitemid.get(i), map.get(menuitemid.get(i)) + 1);
+          }
+      }
+
+      List<BasketItemWithId> Items = new ArrayList<>();
+
+      for (int i = 0; i < array.length; i++) {
+          List<MenuItems> menuItem = mRepo.findByIntegerId(array[i]);
+
+          Integer menuItemId = menuItem.get(0).getItemid();
+          System.out.println(menuItemId);
+          String name = menuItem.get(0).getItemName();
+          Float price = menuItem.get(0).getPrice();
+          Integer quantity = map.get(array[i]);
+
+          BasketItemWithId item = new BasketItemWithId(name, quantity, price * quantity, menuItemId);
+          Items.add(item);
+      }
+      
+      System.out.println("****************************************");
+      System.out.println(Items.size());
+      System.out.println("****************************************");
+      
+      model.addAttribute("Items", Items);
+      
+      return "orderdetail";
+    }
+    
 }

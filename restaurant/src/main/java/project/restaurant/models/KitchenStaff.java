ab5c78c9f50ecf7b433b0aaeeef3f8051ab25015
Tanmeet Gabba (zkac335) <zkac335@live.rhul.ac.uk>
2023-03-02 14:18:07 +0000

    Adding required fields for KitchenStaff model

--- restaurant/src/main/java/project/restaurant/models/KitchenStaff.java
+++ restaurant/src/main/java/project/restaurant/models/KitchenStaff.java
@@ -1,8 +1,20 @@
 package project.restaurant.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name="KitchenStaff")
 public class KitchenStaff {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer kitchenStaffId;
+
+    @Column(name = "permissions", nullable = false)
+    private Integer permissions;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "userid")
+    private Users userid;
 }

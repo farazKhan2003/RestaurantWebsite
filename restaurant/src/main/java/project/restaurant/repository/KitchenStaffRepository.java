0bc0fb31b71e76083de1878a5b4de1cf2b7d8a31
Pengyuan Yan <ykas051@live.rhul.ac.uk>
2023-03-09 04:58:45 +0800

    Add a custom query findKitchenStaffByUID to find the kitchenstaff entity in database by userid

--- restaurant/src/main/java/project/restaurant/repository/KitchenStaffRepository.java
+++ restaurant/src/main/java/project/restaurant/repository/KitchenStaffRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import project.restaurant.models.KitchenStaff;
+import project.restaurant.models.Users;
 
 @Repository
 public interface KitchenStaffRepository extends JpaRepository<KitchenStaff,Integer>{
   @Query(value = "SELECT * FROM kitchen_staff WHERE kitchenstaffid =:kitchenstaffid", nativeQuery = true)
   List<KitchenStaff> findKitchenStaffById(Integer kitchenstaffid);
+  
+  @Query(value = "SELECT * FROM kitchen_staff WHERE userid =:userid", nativeQuery = true)
+  List<KitchenStaff> findKitchenStaffByUID(Integer userid);
 }

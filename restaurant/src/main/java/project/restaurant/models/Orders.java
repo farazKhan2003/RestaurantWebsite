1ce02f24ba9dc834ae53e3e58aa10355e1a9707e
Martin James 2 (2021) ZKAC235 <james.martin.2.2021@live.rhul.ac.uk>
2023-03-09 19:30:52 +0000

    Replace Orders.java to add javadoc and checkstyles

--- restaurant/src/main/java/project/restaurant/models/Orders.java
+++ restaurant/src/main/java/project/restaurant/models/Orders.java
@@ -10,6 +10,12 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
+/**
+ * This class represents an order.
+ *
+ * @author James Faraz Pete
+ */
+
 @Entity
 @Table(name = "Orders")
 public class Orders {
@@ -32,68 +38,155 @@ public class Orders {
   @Column(name = "timeplaced", nullable = false)
   private String timeplaced;
 
-//  @OneToMany(cascade = CascadeType.ALL)
-//  private Set<Itemsorders> itemsorders;
-
   @Column(name = "tablenumber", nullable = false)
   private Integer tablenumber;
 
+  @Column(name = "price", nullable = false)
+  private Float price;
+
+  /**
+   * This constructor builds an order with all its characteristics.
+   * @param state The state the order is in
+   * @param waiterid The ID of the waiter that the order is assigned to
+   * @param userid The ID of the user that placed the order
+   * @param timeplaced The time the user placed the order
+   * @param tablenumber The table number of the user
+   * @param price The price of the order
+   */
   public Orders(String state, Waiters waiterid, Users userid, String timeplaced,
-                Integer tablenumber) {
+                Integer tablenumber, Float price) {
     this.state = state;
     this.waiterid = waiterid;
     this.userid = userid;
     this.timeplaced = timeplaced;
     this.tablenumber = tablenumber;
+    this.price = price;
   }
 
+  /**
+   * This constructor builds an empty order.
+   */
   public Orders() {
 
   }
+
+  /**
+   * This method gets the price of the order.
+   *
+   * @return the price of the order
+   */
+
+  public Float getPrice() {
+    return price;
+  }
+
+  /**
+   * This method sets the price of the order.
+   */
+
+  public void setPrice(Float price) {
+    this.price = price;
+  }
+
+  /**
+   * This method gets the time the order was placed.
+   *
+   * @return the time the order was placed
+   */
 
   public String getTimeplaced() {
     return timeplaced;
   }
 
+  /**
+   * This method sets the time the order was placed.
+   */
+
   public void setTimeplaced(String timeplaced) {
     this.timeplaced = timeplaced;
   }
+
+  /**
+   * This method gets the ID of the order.
+   *
+   * @return the ID of the order
+   */
 
   public Integer getOrderId() {
     return orderid;
   }
 
+  /**
+   * This method sets the price of the order.
+   */
   public void setOrderId(Integer orderid) {
     this.orderid = orderid;
   }
 
+  /**
+   * This method gets the state of the order.
+   *
+   * @return the state of the order
+   */
   public String getState() {
     return state;
   }
+
+  /**
+   * This method sets the state of the order.
+   */
 
   public void setState(String state) {
     this.state = state;
   }
 
+  /**
+   * This method gets the ID of the waiter.
+   *
+   * @return the ID of the waiter
+   */
   public Waiters getWaiterId() {
     return waiterid;
   }
+  /**
+   * This method gets the ID of the waiter.
+   */
 
   public void setWaiterId(Waiters waiterid) {
     this.waiterid = waiterid;
   }
 
+  /**
+   * This method gets the ID of the user.
+   *
+   * @return the ID of the user
+   */
+
   public Users getUserid() {
     return userid;
   }
+
+  /**
+   * This method sets the ID of the user.
+   */
 
   public void setUserid(Users userid) {
     this.userid = userid;
   }
 
+  /**
+   * This method gets the table number of the user.
+   *
+   * @return the table number of the user
+   */
+
   public Integer getTableNumber() {
     return tablenumber;
   }
+
+  /**
+   * This method sets the table number of the user.
+   */
 
   public void setTableNumber(Integer tablenumber) {
     this.tablenumber = tablenumber;

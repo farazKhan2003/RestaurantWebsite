6b95a815283322624704813f5c498b6805230277
Martin James 2 (2021) ZKAC235 <james.martin.2.2021@live.rhul.ac.uk>
2023-03-09 19:28:58 +0000

    Replace ItemsOrders.java with checkstyle and javadoc changes

--- restaurant/src/main/java/project/restaurant/models/ItemsOrders.java
+++ restaurant/src/main/java/project/restaurant/models/ItemsOrders.java
@@ -9,6 +9,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
+/**
+ * This class represents an order.
+ *
+ * @author James Faraz Pete
+ */
 @Entity
 @Table(name = "Itemsorders")
 public class ItemsOrders {
@@ -25,34 +30,66 @@ public class ItemsOrders {
   @JoinColumn(name = "orderid")
   private Orders orderid;
 
+  /**
+   * This constructor will build an empty order.
+   */
   public ItemsOrders() {
   }
 
+  /**
+   * This constructor will build an order and attach it to the menu items ID.
+   */
   public ItemsOrders(MenuItems itemid, Orders orderid) {
     this.itemid = itemid;
     this.orderid = orderid;
   }
 
+  /**
+   * This method gets the ID of the order.
+   *
+   * @return the ID of the order
+   */
+
   public Integer getItemordersid() {
     return itemsordersid;
   }
 
+  /**
+   * This method sets the ID of the order.
+   */
   public void setItemordersid(Integer itemordersid) {
     this.itemsordersid = itemordersid;
   }
+
+  /**
+   * This method gets the ID of the menu item.
+   *
+   * @return the ID of the menu item
+   */
 
   public MenuItems getItemid() {
     return itemid;
   }
 
+  /**
+   * This method sets the ID of the menu item.
+   */
   public void setItemid(MenuItems itemid) {
     this.itemid = itemid;
   }
 
+  /**
+   * This method gets the ID of the order.
+   *
+   * @return the ID of the order
+   */
   public Orders getOrderid() {
     return orderid;
   }
 
+  /**
+   * This method sets the ID of the order.
+   */
   public void setOrderid(Orders orderid) {
     this.orderid = orderid;
   }

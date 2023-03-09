6db8f5162fdc9dd61036e0542837e04fb1c713c8
Khan Faraz (2021) ZKAC354 <faraz.khan.2021@live.rhul.ac.uk>
2023-03-09 19:30:44 +0000

    Controller which moves between pages showing a user all their current orders and characteristics of those orders, also the state of which the order is in.

---
+++ restaurant/src/main/java/project/restaurant/controllers/CurrentOrderController.java
@@ -0,0 +1,56 @@
+package project.restaurant.controllers;
+
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import project.restaurant.models.ItemsOrders;
+import project.restaurant.models.MenuItems;
+import project.restaurant.models.Orders;
+import project.restaurant.models.Users;
+import project.restaurant.repository.ItemsordersRepository;
+import project.restaurant.repository.MenuItemsRepository;
+import project.restaurant.repository.OrdersRepository;
+
+/**
+ * This class will allow a user to view a list of their current orders and the stage it is in.
+ *
+ * @author James Faraz Bailey
+ */
+@Controller
+public class CurrentOrderController {
+
+  @Autowired
+  private OrdersRepository orepo;
+
+  @Autowired
+  private ItemsordersRepository iorepo;
+
+  @Autowired
+  private MenuItemsRepository mrepo;
+
+
+  /**
+   * This method shows a user their current orders and what state it is in.
+   *
+   * @param model   A method to identify a menu item on one webpage
+   * @param session A method to identify a user and waiter across more than one page
+   * @return "currentOrder" The link to the webpage that displays the information of a users orders
+   */
+  @GetMapping("/currentOrder")
+  public String getCurrentOrder(Model model, HttpSession session) {
+    Users user = (Users) session.getAttribute("user");
+    List<Orders> orders = orepo.findbyStateAndID("done", user);
+    List<ItemsOrders> itemsOrdered = iorepo.findAllItemOrders(orders.get(0));
+    List<MenuItems> menuItem = new ArrayList<>();
+    for (ItemsOrders item : itemsOrdered) {
+      menuItem.add(item.getItemid());
+    }
+    model.addAttribute("orders", orders);
+    model.addAttribute("menuitems", menuItem);
+    return "currentOrder";
+  }
+}

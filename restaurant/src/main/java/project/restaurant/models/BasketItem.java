145b3097448dfafbbb780b44e4a4104f36163bf4
Yan Pengyuan (2021) YKAS051 <pengyuan.yan.2021@live.rhul.ac.uk>
2023-03-13 20:52:30 +0000

    Update BasketItem.java add checkstyle and javadoc

--- restaurant/src/main/java/project/restaurant/models/BasketItem.java
+++ restaurant/src/main/java/project/restaurant/models/BasketItem.java
@@ -2,49 +2,51 @@ package project.restaurant.models;
 
 /**
  * This class represents an item from the menu in the customer's basket.
+ * 
+ * @author Tanmeet Kenny Wen
  */
 public class BasketItem {
-    private String name;
-    private Integer quantity;
-    private Float priceSum;
+  private final String name;
+  private final Integer quantity;
+  private final Float priceSum;
 
-    /**
-     * Constructs a BasketItem from the menu.
-     *
-     * @param name the name of the item
-     * @param quantity the number of this item the customer has added to the basket
-     * @param priceSum the total value of this item according to the quantity (price * quantity)
-     */
-    public BasketItem(String name, Integer quantity, Float priceSum) {
-        this.name = name;
-        this.quantity = quantity;
-        this.priceSum = priceSum;
-    }
+  /**
+   * Constructs a BasketItem from the menu.
+   *
+   * @param name the name of the item
+   * @param quantity the number of this item the customer has added to the basket
+   * @param priceSum the total value of this item according to the quantity (price * quantity)
+   */
+  public BasketItem(String name, Integer quantity, Float priceSum) {
+    this.name = name;
+    this.quantity = quantity;
+    this.priceSum = priceSum;
+  }
 
-    /**
-     * This method gets the name of item.
-     *
-     * @return the name of the item
-     */
-    public String getName() {
-        return name;
-    }
+  /**
+   * This method gets the name of item.
+   *
+   * @return the name of the item
+   */
+  public String getName() {
+    return name;
+  }
 
-    /**
-     * This method gets the quantity of the item.
-     *
-     * @return the quantity of the item
-     */
-    public int getQuantity() {
-        return quantity;
-    }
+  /**
+   * This method gets the quantity of the item.
+   *
+   * @return the quantity of the item
+   */
+  public int getQuantity() {
+    return quantity;
+  }
 
-    /**
-     * This method gets the the sum price of the item
-     *
-     * @return the total price according to the quantity
-     */
-    public float getPriceSum() {
-        return priceSum;
-    }
+  /**
+   * This method gets the sum price of the item
+   *
+   * @return the total price according to the quantity
+   */
+  public float getPriceSum() {
+    return priceSum;
+  }
 }

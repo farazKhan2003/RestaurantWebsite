ef3afb44a2443a6b63eb05dd63a854f15fd0b1a5
Pengyuan Yan <ykas051@live.rhul.ac.uk>
2023-03-16 07:05:30 +0800

    Add a getMapping method for customerCancelOrders web page

--- restaurant/src/main/java/project/restaurant/controllers/CancelOrderController.java
+++ restaurant/src/main/java/project/restaurant/controllers/CancelOrderController.java
@@ -1,8 +1,28 @@
 package project.restaurant.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.http.HttpSession;
+import project.restaurant.models.Orders;
+import project.restaurant.models.Users;
+import project.restaurant.models.Waiters;
+import project.restaurant.repository.OrdersRepository;
 
 @Controller
 public class CancelOrderController {
-
+  
+  @Autowired
+  private OrdersRepository orepo;
+  
+  @GetMapping("/customerCancelOrders")
+  public String getCancelOrders(Model model, HttpSession session) {
+    Users user = (Users) session.getAttribute("user");
+    List<Orders> cancelOrder = orepo.findNotComfirmedAndComfirmedOrdersByUserId(user);
+    model.addAttribute("cancelOrder", cancelOrder);
+    return "customerCancelOrders";
+  }
 }

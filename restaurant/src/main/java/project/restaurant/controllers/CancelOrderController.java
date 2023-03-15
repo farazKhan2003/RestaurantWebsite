83cbf29ee87b7d44ecc812dbc28a363936cde807
Pengyuan Yan <ykas051@live.rhul.ac.uk>
2023-03-16 07:35:04 +0800

    Add a new postmapping method changeStateToDelivered for customerCancelOrders web page to start a order canceling request

--- restaurant/src/main/java/project/restaurant/controllers/CancelOrderController.java
+++ restaurant/src/main/java/project/restaurant/controllers/CancelOrderController.java
@@ -3,9 +3,11 @@ package project.restaurant.controllers;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import jakarta.servlet.http.HttpSession;
 import project.restaurant.models.Orders;
 import project.restaurant.models.Users;
@@ -23,6 +25,15 @@ public class CancelOrderController {
     Users user = (Users) session.getAttribute("user");
     List<Orders> cancelOrder = orepo.findNotComfirmedAndComfirmedOrdersByUserId(user);
     model.addAttribute("cancelOrder", cancelOrder);
+    return "customerCancelOrders";
+  }
+  
+  @PostMapping("/tryToCancel")
+  public String changeStateToDelivered(@Param("input") Integer input, Model model, HttpSession session) {
+    Orders order = orepo.findOrderByOrderId(input);
+    order.setState("canceling");
+    orepo.save(order);
+    getCancelOrders(model, session);
     return "customerCancelOrders";
   }
 }
